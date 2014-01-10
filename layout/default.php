b5f2456fd5a0c8b45c1fcb3745900f15b81339b2
Julian Ridden <julian@moodleman.net>
2014-01-10 22:06:25 +1100

    adding back to top button

--- layout/default.php
+++ layout/default.php
@@ -106,5 +106,25 @@ echo $OUTPUT->doctype() ?>
     $("#b-100").click(function() { NProgress.done(); });
 </script>
 
+<script type="text/javascript">
+jQuery(document).ready(function() {
+    var offset = 220;
+    var duration = 500;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.back-to-top').fadeIn(duration);
+        } else {
+            jQuery('.back-to-top').fadeOut(duration);
+        }
+    });
+    
+    jQuery('.back-to-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+});
+</script>
+ <a href="#top" class="back-to-top"><i class="fa fa-angle-up "></i></a>
 </body>
 </html>
