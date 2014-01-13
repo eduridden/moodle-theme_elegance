873660bce683958034ced8ce3dff366aaa1837e1
Julian Ridden <julian@moodleman.net>
2014-01-13 13:14:51 +1100

    Bump version number

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2014011100;
-$plugin->release = '1.0 (Build: 20140111)';
+$plugin->version   = 2014011301;
+$plugin->release = '1.0 (Build: 20140113)';
 $plugin->maturity = MATURITY_STABLE;             // this version's maturity level.
 $plugin->requires  = 2013110500;
 $plugin->component = 'theme_elegance';
